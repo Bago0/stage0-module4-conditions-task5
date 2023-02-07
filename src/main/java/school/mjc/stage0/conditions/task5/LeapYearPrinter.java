e37b4611b0416176ab29c0ce3c484e21ae9d23a9
Bago0 <jgamadzebagrati@gmail.com>
2023-02-07 19:30:25 +0400

    conditions task5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int check = year % 4;
+        switch (check){
+            case 0:
+                System.out.println("leap");
+            default:
+                System.out.println("not leap");
+        }
     }
 }
